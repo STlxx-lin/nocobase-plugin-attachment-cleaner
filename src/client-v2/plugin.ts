14979d13b1075225e11a9dc2d5ee7545666040d4
STlxx <1414794992@qq.com>
2026-09-11 15:32:20 +0800

    fix(client-v2): add modern settings registration and bidirectional link support

--- src/client-v2/plugin.ts
+++ src/client-v2/plugin.ts
@@ -7,10 +7,59 @@
  * For more information, please refer to: https://www.nocobase.com/agreement.
  */
 
+import React from 'react';
 import { Plugin } from '@nocobase/client-v2';
+import { AttachmentCleanerPage } from '../client/AttachmentCleanerPage';
 
 export class PluginAttachmentCleanerClient extends Plugin {
-  async load() {}
+  async load() {
+    const manager = this.app.pluginSettingsManager as any;
+    if (!manager) return;
+
+    const title = '附件清理管理';
+    const icon = 'DeleteOutlined';
+    const menuKey = 'attachment-cleaner';
+    const pageName = `${menuKey}.index`;
+
+    if (typeof manager.addMenuItem === 'function' && typeof manager.addPageTabItem === 'function') {
+      manager.addMenuItem({
+        key: menuKey,
+        title,
+        icon,
+        aclSnippet: 'pm',
+      });
+
+      manager.addPageTabItem({
+        menuKey,
+        key: 'index',
+        title,
+        icon,
+        aclSnippet: 'pm',
+        Component: AttachmentCleanerPage,
+      });
+
+      const pluginNames = [
+        this.options?.name,
+        this.options?.packageName,
+        'attachment-cleaner',
+        '@nocobase/plugin-attachment-cleaner',
+      ].filter(Boolean);
+
+      [...new Set(pluginNames)].forEach((pluginName) => {
+        manager.setPluginSettingsLink?.(pluginName, pageName);
+      });
+      return;
+    }
+
+    if (typeof manager.add === 'function') {
+      manager.add(menuKey, {
+        title,
+        icon,
+        aclSnippet: 'pm',
+        Component: AttachmentCleanerPage,
+      });
+    }
+  }
 }
 
 export default PluginAttachmentCleanerClient;
--- src/client/plugin.ts
+++ src/client/plugin.ts
@@ -1,12 +1,52 @@
+import React from 'react';
 import { Plugin } from '@nocobase/client';
 import { AttachmentCleanerPage } from './AttachmentCleanerPage';
 
 export class PluginAttachmentCleanerClient extends Plugin {
   async load() {
-    if (this.app?.pluginSettingsManager) {
-      this.app.pluginSettingsManager.add('attachment-cleaner', {
-        title: '附件清理管理',
-        icon: 'DeleteOutlined',
+    const manager = this.app?.pluginSettingsManager as any;
+    if (!manager) return;
+
+    const title = '附件清理管理';
+    const icon = 'DeleteOutlined';
+    const menuKey = 'attachment-cleaner';
+    const pageName = `${menuKey}.index`;
+
+    if (typeof manager.addMenuItem === 'function' && typeof manager.addPageTabItem === 'function') {
+      manager.addMenuItem({
+        key: menuKey,
+        title,
+        icon,
+        aclSnippet: 'pm',
+      });
+
+      manager.addPageTabItem({
+        menuKey,
+        key: 'index',
+        title,
+        icon,
+        aclSnippet: 'pm',
+        Component: AttachmentCleanerPage,
+      });
+
+      const pluginNames = [
+        this.options?.name,
+        this.options?.packageName,
+        'attachment-cleaner',
+        '@nocobase/plugin-attachment-cleaner',
+      ].filter(Boolean);
+
+      [...new Set(pluginNames)].forEach((pluginName) => {
+        manager.setPluginSettingsLink?.(pluginName, pageName);
+      });
+      return;
+    }
+
+    if (typeof manager.add === 'function') {
+      manager.add(menuKey, {
+        title,
+        icon,
+        aclSnippet: 'pm',
         Component: AttachmentCleanerPage,
       });
     }
